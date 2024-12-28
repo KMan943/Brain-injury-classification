536ce6472bb392f85c98f68f63e598cff469746d
KMan943 <kman05108@gmail.com>
2024-12-28 15:38:14 +0530

    model_changes and final touches

--- haemorrhage_detector/base/views.py
+++ haemorrhage_detector/base/views.py
@@ -3,50 +3,74 @@ from base.forms import PatientForm
 import tensorflow as tf
 import cv2
 import numpy as np
+from xgboost import XGBClassifier
 
+models_download_link = 'https://drive.google.com/drive/folders/1OFaAADrsEOCJl-WGB8BzeXlsnME-2dFG?usp=sharing'
 
-brain_model_download_link = 'https://drive.google.com/file/d/1-5HFTklEXsAiF5ovF1hk7xNGtkTAqY3s/view?usp=sharing'
-bone_model_download_link = 'https://drive.google.com/file/d/1-3QoEf5YWJhnbqvHvqpEdk0dcuwHwPz_/view?usp=sharing'
-
-brain_model_path = r'C:\Users\kman0\Downloads\college projects\siddhi 2.0\models\Res101-brainModel.keras'
-bone_model_path = r'C:\Users\kman0\Downloads\college projects\siddhi 2.0\models\Res101-boneModel.keras'
+feature_model_path = r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\feature_gen.keras"
+bone_model_path = r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\models\Res101-boneModel.keras"
 # saved_brain_model_layer = tf.keras.layers.TFSMLayer(brain_model_path, call_endpoint="serving_default")
 # saved_bone_model_layer = tf.keras.layers.TFSMLayer(bone_model_path, call_endpoint="serving_default")
 
 # brain_model = tf.keras.Sequential([saved_brain_model_layer])
 # bone_model = tf.keras.Sequential([saved_bone_model_layer])
 
-brain_model = tf.keras.models.load_model(brain_model_path)
+feature_model = tf.keras.models.load_model(feature_model_path)
 bone_model = tf.keras.models.load_model(bone_model_path)
 
 def make_brain_prediction(image_path):
     img = cv2.imread(image_path)
     img_array = np.array([img])
-    predictions = brain_model.predict(img_array)
+    features = feature_model.predict(img_array)
      
-    predicted_class = np.argmax(predictions[0])  
-    if predicted_class==0 :
-        haemorrhage = 'Intraventricular' 
-    elif predicted_class==1 :
-        haemorrhage = 'Intraparenchymal'
-    elif predicted_class==2 :
-        haemorrhage = 'Subarachnoid'
-    elif predicted_class==3 :
-        haemorrhage = 'Epidural'
-    elif predicted_class==4 :
-        haemorrhage = 'Subdural'
-    elif predicted_class==5 :
-        haemorrhage = 'No'
+    
+    xgb_model_iv = XGBClassifier()
+    xgb_model_iv.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_iv.bin")
+    iv = xgb_model_iv.predict(features)[0]
 
-    predicted_probability = predictions[0][predicted_class]  
+    xgb_model_ip = XGBClassifier()
+    xgb_model_ip.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_ip.bin")
+    ip = xgb_model_ip.predict(features)[0]
 
-    return haemorrhage, predicted_probability
+    xgb_model_sa = XGBClassifier()
+    xgb_model_sa.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_sa.bin")
+    sa = xgb_model_sa.predict(features)[0]
+
+    xgb_model_ep = XGBClassifier()
+    xgb_model_ep.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_ep.bin")
+    ep = xgb_model_ep.predict(features)[0]
+
+    xgb_model_sd = XGBClassifier()
+    xgb_model_sd.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_sd.bin")
+    sd = xgb_model_sd.predict(features)[0]
+
+    xgb_model_no = XGBClassifier()
+    xgb_model_no.load_model(r"C:\Users\kman0\Downloads\college projects\siddhi 2.0\XGB_models\xg_model_no.bin")
+    no = xgb_model_no.predict(features)[0]
+
+
+    haemorrhage = []
+    if iv==1 :
+        haemorrhage.append('Intraventricular Haemorrhage') 
+    if ip==1 :
+        haemorrhage.append('Intraparenchymal Haemorrhage')
+    if sa==1 :
+        haemorrhage.append('Subarachnoid Haemorrhage')
+    if ep==1 :
+        haemorrhage.append('Epidural Haemorrhage')
+    if sd==1 :
+        haemorrhage.append('Subdural Haemorrhage')
+    if no==1 :
+        haemorrhage = ['No Haemorrhage']
+
+
+    return haemorrhage
 
 def make_bone_prediction(image_path):
     img = cv2.imread(image_path)
     img_array = np.array([img])
     predictions = bone_model.predict(img_array)
-    predicted_probability = predictions[0]
+    predicted_probability = predictions[0][0]
     prob = predicted_probability
     if predicted_probability < 0.5:
         bone_pred = "No Fracture"
@@ -65,13 +89,13 @@ def predict(request):
             brain_image_path = img.brain_image.path
             bone_image_path = img.bone_image.path
 
-            brain_pred_class , brain_pred_prob = make_brain_prediction(brain_image_path)
+            haemorrhage = make_brain_prediction(brain_image_path)
             bone_pred , bone_pred_prob = make_bone_prediction(bone_image_path)
 
             # print(bone_image_path)
             # print(brain_image_path)
 
-            context = {'brain_img' : brain_image_path , 'bone_img' : bone_image_path , 'brain_pred_class' : brain_pred_class  , 'brain_pred_prob' : brain_pred_prob , 'bone_pred' : bone_pred , 'bone_pred_prob' : bone_pred_prob}
+            context = {'brain_img' : brain_image_path , 'bone_img' : bone_image_path , 'haemorrhage' : haemorrhage  , 'bone_pred' : bone_pred , 'bone_pred_prob' : bone_pred_prob}
             return render(request , 'base/results.html' , context=context)
 
         else:
